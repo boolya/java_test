13d8308061d9157874ad8f933d19d1484145dc85
boolya <borisova.olga.383@gmail.com>
2016-07-17 15:31:38 +0600

    Добавлено несколько функций

--- sandbox/src/main/java/pack/java/prog/sandbox/Hello.java
+++ sandbox/src/main/java/pack/java/prog/sandbox/Hello.java
@@ -3,7 +3,28 @@ package pack.java.prog.sandbox;
 public class Hello {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hellofu("world");
+    hellofu("user");
+    hellofu("Olya");
+
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+  }
+
+  public static void hellofu(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area (double len){
+    return len * len;
+  }
+
+  public static double area (double a, double b){
+    return a * b;
   }
 
 }
