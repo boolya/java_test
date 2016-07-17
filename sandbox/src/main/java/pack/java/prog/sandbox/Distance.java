fdb6bfc5d41b0ad9940e3cb7ab51b953a1dcd5e3
boolya <borisova.olga.383@gmail.com>
2016-07-17 17:59:36 +0600

    HW_2. п.4 с примерами использования метода

--- sandbox/src/main/java/pack/java/prog/sandbox/Distance.java
+++ sandbox/src/main/java/pack/java/prog/sandbox/Distance.java
@@ -6,7 +6,19 @@ package pack.java.prog.sandbox;
 
 public class Distance {
   public static void main(String[] args) {
-    Point p = new Point(1,1,3,3);
-    System.out.println("ДОМАШНЕЕ ЗАДАНИЕ №2. Расстояние между точками " + "p1" + " с координатами (" + p.x1 + "," + p.y1 + ") и p2" + " с координатами (" + p.x2 + "," + p.y2 + ") равно " + p.distance() + ".");
+    System.out.println("ДОМАШНЕЕ ЗАДАНИЕ №2. Вычисление расстояния между двумя точками.\n");
+
+    printDistance(1,1,3,3);
+    printDistance(0,0,10,0);
+    printDistance(1.5,5.76,-1,-12.6);
+    printDistance(10,0,0,25);
+    printDistance(-5.5,10,13,-0.3);
+  }
+
+  public static void printDistance(double p1x, double p1y, double p2x, double p2y) {
+    Point p = new Point(p1x, p1y, p2x, p2y);
+    System.out.println("Координаты точки p1: " + "(" + p.x1 + ", " + p.y1 + ")");
+    System.out.println("Координаты точки p2: " + "(" + p.x2 + ", " + p.y2 + ")");
+    System.out.println("Расстояние между точками: " + p.distance() + ".\n");
   }
 }
