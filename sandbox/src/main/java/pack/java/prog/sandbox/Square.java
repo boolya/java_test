f374080fa2b7ab5e8826f3156839f54531ac9a3d
boolya <borisova.olga.383@gmail.com>
2016-07-17 16:07:33 +0600

    Ещё немного махинаций с классами и методами

---
+++ sandbox/src/main/java/pack/java/prog/sandbox/Square.java
@@ -0,0 +1,17 @@
+package pack.java.prog.sandbox;
+
+/**
+ * Created by Оля on 17.07.2016.
+ */
+public class Square {
+  public double l;
+
+  public Square(double l){
+    this.l = l;
+  }
+
+  public double area (){
+    return this.l * this.l;
+  }
+
+}
