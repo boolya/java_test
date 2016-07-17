2ee2b97a0c773cbe434964b4728c59d0d9586ae4
boolya <borisova.olga.383@gmail.com>
2016-07-17 17:25:57 +0600

    HW_2. п.4 с одним примером

--- sandbox/src/main/java/pack/java/prog/sandbox/Distance.java
+++ sandbox/src/main/java/pack/java/prog/sandbox/Distance.java
@@ -6,12 +6,7 @@ package pack.java.prog.sandbox;
 
 public class Distance {
   public static void main(String[] args) {
-    Point p1 = new Point(1,1);
-    Point p2 = new Point(3,3);
-    System.out.println("ДОМАШНЕЕ ЗАДАНИЕ №2. Расстояние между точками " + "p1" + " с координатами (" + p1.x + "," + p1.y + ") и p2" + " с координатами (" + p2.x + "," + p2.y + ") равно " + distance(p1, p2) + ".");
-  }
-
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt(Math.pow((p2.x - p1.x),2) + Math.pow((p2.y - p1.y),2));
+    Point p = new Point(1,1,3,3);
+    System.out.println("ДОМАШНЕЕ ЗАДАНИЕ №2. Расстояние между точками " + "p1" + " с координатами (" + p.x1 + "," + p.y1 + ") и p2" + " с координатами (" + p.x2 + "," + p.y2 + ") равно " + p.distance() + ".");
   }
 }
--- sandbox/src/main/java/pack/java/prog/sandbox/Point.java
+++ sandbox/src/main/java/pack/java/prog/sandbox/Point.java
@@ -5,11 +5,21 @@ package pack.java.prog.sandbox;
  */
 
 public class Point {
-  public double x; //координата точки по оси OX
-  public double y; //координата точки по оси OY
+  public double x1; //координата точки p1 по оси OX
+  public double y1; //координата точки p1 по оси OY
 
-  public Point(double x, double y){
-    this.x = x;
-    this.y = y;
+  public double x2; //координата точки p2 по оси OX
+  public double y2; //координата точки p2 по оси OY
+
+  public Point(double x1, double y1, double x2, double y2){
+    this.x1 = x1;
+    this.y1 = y1;
+
+    this.x2 = x2;
+    this.y2 = y2;
+  }
+
+  public double distance(){
+    return Math.sqrt(Math.pow((this.x1 - this.x2),2) + Math.pow((this.y1 - this.y2),2));
   }
 }
