ed896197a2130748d390a26e7d9ca345c59dac59
boolya <borisova.olga.383@gmail.com>
2016-07-30 16:48:48 +0700

    Реализована проверка и обеспечение предусловий для тестов удаления и изменения групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -8,6 +8,7 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
+    //app.getGroupHelper().createGroup(new GroupData("test1", null, null)) //альтернативный вариант взамен следующих 4 строчек;
     app.getGroupHelper().initGroupCreation();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
     app.getGroupHelper().submitGroupCreation();
