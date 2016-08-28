6ae2fa345b035e6adf7c51cfa79ad5cdfe26e16d
boolya <borisova.olga.383@gmail.com>
2016-08-28 18:42:32 +0700

    HW_14. Генераторы тестовых данных в формате json, и HW_15. Датапровайдеры для их чтения (для тестов создания групп и контактов)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
@@ -11,17 +12,27 @@ public class ContactData {
 
   @XStreamOmitField
   private int id = Integer.MAX_VALUE;
-
+  @Expose
   private String firstName;
+  @Expose
   private String lastName;
+  @Expose
   private String address;
+  @Expose
   private String phoneHome;
+  @Expose
   private String phoneMobile;
+  @Expose
   private String phoneWork;
+  @Expose
   private String email1;
+  @Expose
   private String email2;
+  @Expose
   private String email3;
+  @Expose
   private String group;
+
   private String allPhones;
   private String allEmails;
   private String contactInfo;
