afa468b4e781fd046b4aafc37ae17cd75838a6e5
boolya <borisova.olga.383@gmail.com>
2016-09-10 17:35:02 +0700

    MantisBT. SoapTests. TestCreateIssue

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SoapHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/SoapHelper.java
@@ -1,11 +1,11 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
-import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
-import biz.futureware.mantis.rpc.soap.client.ProjectData;
+import biz.futureware.mantis.rpc.soap.client.*;
+import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
 
 import javax.xml.rpc.ServiceException;
+import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
@@ -22,13 +22,34 @@ public class SoapHelper {
   }
 
   public Set<Project> getProjects() throws MalformedURLException, ServiceException, RemoteException {
-    MantisConnectPortType mc = new MantisConnectLocator()
-            .getMantisConnectPort(new URL("http://localhost/mantisbt-1.3.0/api/soap/mantisconnect.php"));
-    ProjectData[] projects = mc.mc_projects_get_user_accessible("administrator", "root");
+    MantisConnectPortType mc = getMantisConnect();
+    ProjectData[] projects = mc.mc_projects_get_user_accessible(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"));
     return Arrays.asList(projects).stream()
             .map((p) -> new Project().withId(p.getId().intValue()).withName(p.getName()))
             .collect(Collectors.toSet());
   }
 
+  private MantisConnectPortType getMantisConnect() throws ServiceException, MalformedURLException, RemoteException {
+    return new MantisConnectLocator().getMantisConnectPort(new URL(app.getProperty("soap.url")));
+  }
 
+
+  public Issue addIssue(Issue issue) throws RemoteException, ServiceException, MalformedURLException {
+    MantisConnectPortType mc = getMantisConnect();
+    String[] categories = mc.mc_project_get_categories(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"), BigInteger.valueOf(issue.getProject().getId()));
+    IssueData issueData = new IssueData();
+    issueData.setSummary(issue.getSummary());
+    issueData.setDescription(issue.getDescription());
+    issueData.setProject(new ObjectRef(BigInteger.valueOf(issue.getProject().getId()), issue.getProject().getName()));
+    issueData.setCategory(categories[0]);
+    BigInteger issuedId = mc.mc_issue_add(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"), issueData);
+    IssueData createdIssueData = mc.mc_issue_get(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword"), issuedId);
+    return new Issue()
+            .withId(createdIssueData.getId().intValue())
+            .withDescription(createdIssueData.getDescription())
+            .withSummary(createdIssueData.getSummary())
+            .withProject(new Project()
+                    .withId(createdIssueData.getProject().getId().intValue())
+                    .withName(createdIssueData.getProject().getName()));
+  }
 }
